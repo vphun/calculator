50af44f8256f4afff52d25c631508e4e91736eef
vphun <vphun@hmc.edu>
2019-07-19 23:21:04 -0700

    Added queries for buttons

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+let currentDisplay = "";
+
+const digits = document.querySelectorAll(".digits button");
+digits.forEach(digit=>digit.addEventListener("click", e => {
+    console.log(e.target.value);
+    currentDisplay += e.target.value;
+    updateDisplay(currentDisplay);
+}));
+
+const operations = document.querySelectorAll(".operations button");
+operations.forEach(operation=>operation.addEventListener("click", e =>{
+    console.log(e.target.value);
+
+
+}));
+
+function updateDisplay(display) {
+    const displayDiv = document.querySelector(".display");
+    displayDiv.textContent = display;
+}
+
+function clearScreen() {
+    displayDiv.textContent = "0";
+}
+
 function add(x, y) {
-    return x + y;
+    return +x + +y;
 }
 
 function subtract(x, y) {
@@ -14,6 +39,13 @@ function divide(x, y) {
     return x / y;
 }
 
+function switchSign(x) {
+    return -x;
+}
+
+function percentToDecimal(x) {
+    return x/100;
+}
 function operate(operator, x, y) {
     return operator(x, y);
 }
